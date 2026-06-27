20429aca7ebb594aa8b8c81c5855bbef68e3eb4e
brilliantgibrann <brilliantgibr.j@cakrawala.ac.id>
2026-06-27 17:24:24 +0700

    ci: remove node api routes and clean next config for perfect static export

--- next.config.js
+++ next.config.js
@@ -4,27 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  trailingSlash: true,
-  async headers() {
-    const csp = [
-      "default-src 'self'",
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-      "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net9 https://unpkg.com",
-      "img-src 'self' data: blob:",
-      "font-src 'self' https://fonts.gstatic.com"
-    ];
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: csp.join('; ')
-          }
-        ]
-      }
-    ];
-  }
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
